8df4b7ab38777d3596329d3c10d6e100f9c73def
zZerooneXx <zZerooneXx@gmail.com>
2021-06-04 03:57:28 +0300

    check_2

--- src/iptables.sh
+++ src/iptables.sh
@@ -69,10 +69,6 @@ else
   fi
 fi
 
-#########################################################################################
-################################### REMOVE FIREWALLD ####################################
-#########################################################################################
-
 if _exists "firewalld"; then
   _proc "uninstalling the firewalld..."
   systemctl disable firewalld >/dev/null 2>&1
@@ -81,10 +77,6 @@ if _exists "firewalld"; then
   _success "firewalld successfully uninstalled!"
 fi
 
-#########################################################################################
-################################### INSTALL IPTABLES ####################################
-#########################################################################################
-
 if ! _exists "iptables"; then
   _proc "installing the iptables..."
   dnf -qy install iptables-services
@@ -92,19 +84,11 @@ if ! _exists "iptables"; then
   _success "iptables successfully installed!"
 fi
 
-#########################################################################################
-##################################### ANTI SPOOFING #####################################
-#########################################################################################
-
 if [ -e /proc/sys/net/ipv4/conf/all/rp_filter ]; then
   for filter in /proc/sys/net/ipv4/conf/*/rp_filter; do
     echo 1 >"$filter"
   done
 fi
-
-#########################################################################################
-#################################### DEFAULT POLICY #####################################
-#########################################################################################
 
 # clear all rules
 iptables -F
@@ -129,10 +113,6 @@ iptables -A INPUT -i lo -j ACCEPT
 # allow established TCP connections:
 iptables -A INPUT -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-#########################################################################################
-############################### Anti-Attack: Stealth Scan ###############################
-#########################################################################################
-
 iptables -N STEALTH_SCAN # make a chain with the name "STEALTH_SCAN"
 iptables -A STEALTH_SCAN -j LOG --log-prefix "stealth_scan_attack: "
 iptables -A STEALTH_SCAN -j DROP
@@ -150,16 +130,8 @@ iptables -A INPUT -p tcp --tcp-flags ACK,FIN FIN -j STEALTH_SCAN
 iptables -A INPUT -p tcp --tcp-flags ACK,PSH PSH -j STEALTH_SCAN
 iptables -A INPUT -p tcp --tcp-flags ACK,URG URG -j STEALTH_SCAN
 
-#########################################################################################
-############# Anti-Attack: Port scanning with fragment packets, DOS attacks #############
-#########################################################################################
-
 iptables -A INPUT -f -j LOG --log-prefix 'fragment_packet:'
 iptables -A INPUT -f -j DROP
-
-#########################################################################################
-############################## Anti-Attack: Ping of Death ###############################
-#########################################################################################
 
 iptables -N PING_OF_DEATH # make a chain with the name "PING_OF_DEATH"
 iptables -A PING_OF_DEATH -p icmp --icmp-type echo-request \
@@ -178,10 +150,6 @@ iptables -A PING_OF_DEATH -j DROP
 # ICMP jumps to "PING_OF_DEATH" chain
 iptables -A INPUT -p icmp --icmp-type echo-request -j PING_OF_DEATH
 
-#########################################################################################
-############################# Anti-Attack: SYN Flood Attack #############################
-#########################################################################################
-
 iptables -N SYN_FLOOD # make a chain with the name "SYN_FLOOD"
 iptables -A SYN_FLOOD -p tcp --syn \
 -m hashlimit \
@@ -199,23 +167,28 @@ iptables -A SYN_FLOOD -j DROP
 # SYN packets jump to the "SYN_FLOOD" chain
 iptables -A INPUT -p tcp --syn -j SYN_FLOOD
 
-#########################################################################################
-############################# Anti-Attack: IDENT port probe #############################
-#########################################################################################
+iptables -N HTTP_DOS # make a chain with the name "HTTP_DOS"
+iptables -A HTTP_DOS -p tcp -m multiport --dports $HTTP \
+-m hashlimit \
+--hashlimit 1/s \
+--hashlimit-burst 100 \
+--hashlimit-htable-expire 300000 \
+--hashlimit-mode srcip \
+--hashlimit-name t_HTTP_DOS \
+-j RETURN
+
+# discard connections that exceed the limit
+iptables -A HTTP_DOS -j LOG --log-prefix "http_dos_attack: "
+iptables -A HTTP_DOS -j DROP
+
+# packets to HTTP jump to the "HTTP_DOS" chain
+iptables -A INPUT -p tcp -m multiport --dports $HTTP -j HTTP_DOS
 
 iptables -A INPUT -p tcp -m multiport --dports 113 -j REJECT --reject-with tcp-reset
-
-#########################################################################################
-############################# Anti-Attack: SSH Brute Force ##############################
-#########################################################################################
 
 iptables -A INPUT -p tcp --syn -m multiport --dports $SSH -m recent --name ssh_attack --set
 iptables -A INPUT -p tcp --syn -m multiport --dports $SSH -m recent --name ssh_attack --rcheck --seconds 180 --hitcount 8 -j LOG --log-prefix "ssh_brute_force: "
 iptables -A INPUT -p tcp --syn -m multiport --dports $SSH -m recent --name ssh_attack --rcheck --seconds 180 --hitcount 8 -j REJECT --reject-with tcp-reset
-
-#########################################################################################
-## Packets destined for all hosts (broadcast address, multicast address) are discarded ##
-#########################################################################################
 
 iptables -A INPUT -d 192.168.1.255 -j LOG --log-prefix "drop_broadcast: "
 iptables -A INPUT -d 192.168.1.255 -j DROP
@@ -223,10 +196,6 @@ iptables -A INPUT -d 255.255.255.255 -j LOG --log-prefix "drop_broadcast: "
 iptables -A INPUT -d 255.255.255.255 -j DROP
 iptables -A INPUT -d 224.0.0.1 -j LOG --log-prefix "drop_broadcast: "
 iptables -A INPUT -d 224.0.0.1 -j DROP
-
-#########################################################################################
-############################ Input permission from all hosts ############################
-#########################################################################################
 
 # ICMP
 iptables -A INPUT -p icmp -j ACCEPT
@@ -255,10 +224,6 @@ if _exists "ragemp"; then
   iptables -A INPUT -p tcp -m multiport --dports $RAGEMP -j ACCEPT
   iptables -A INPUT -p udp -m multiport --dports $RAGEMP -j ACCEPT
 fi
-
-#########################################################################################
-############ Log and discard anything that does not apply to the above rules ############
-#########################################################################################
 
 iptables -A INPUT -j LOG --log-prefix "drop: "
 iptables -A INPUT -j DROP
